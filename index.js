ec4c9c74ac56fb21e8b807a9c172769a68a4549f
Adriana <adriana.amaral@msn.com>
2021-04-05 17:33:21 -0500

    commit tree 70f75d49330b5f6cb64633867f50acdc7f663076

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString) {
+  let time = timeString.split (':'); 
+  //format
+  let currentTime = parseInt(time);
+  //function parses a string argument and returns an integer
+if (currentTime<12){
+    return "Good Morning";
+  } else if (currentTime>17){
+    return "Good Evening";
+  }
+  return "Good Afternoon";
+}
+
+
+function displayMessage(message)  {
+
+  document.getElementById("greeting").innerText = message;
+//# > ID | . > tag  #greeting node 
+}
+
+
